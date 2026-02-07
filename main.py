3e5fd12f6037dd52e69e2d585046f8610cdc616a
Rutuja-Ulpe <rutujaulpe03@gmail.com>
2026-02-07 15:29:42 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+
+from src.data_preprocessing import load_and_preprocess
+from src.train_model import train
+from src.evaluate_model import evaluate
+
+def main():
+    X_train, X_test, y_train, y_test = load_and_preprocess()
+    model = train(X_train, y_train)
+    evaluate(model, X_test, y_test)
+
+if __name__ == "__main__":
+    main()
